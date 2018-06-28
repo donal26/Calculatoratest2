0c7a15ad2f745e7e08251d78f24b5c0e4d0b532e
donal26 <40645627+donal26@users.noreply.github.com>
2018-06-28 17:57:14 -0500

    Update CalculateTest.php

--- tests/CalculateTest.php
+++ tests/CalculateTest.php
@@ -7,7 +7,7 @@ final class CalculateTest extends TestCase{
 	public function testAdd():void{
 		include_once "src/calculate.php";
 		$calculate=new Calculate();
-		$this->assertEquals(  $calculate->add(8,7), 12);
+		$this->assertEquals(  $calculate->add(8,7), 15);
 	}
 
 	public function testDivide():void{
