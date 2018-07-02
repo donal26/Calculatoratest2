eba616e29cf51dbedaef2fd869d37af1773bfa59
AbelMiraval <abelmiraval@gmail.com>
2018-07-02 14:07:06 -0500

    Modificando suma

--- tests/CalculateTest.php
+++ tests/CalculateTest.php
@@ -7,7 +7,7 @@ final class CalculateTest extends TestCase{
 	public function testAdd():void{
 		include_once "src/calculate.php";
 		$calculate=new Calculate();
-		$this->assertEquals(  $calculate->add(8,7), 15);
+		$this->assertEquals(  $calculate->add(80,80), 160);
 	}
 
 	public function testDivide():void{
